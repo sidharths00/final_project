f76294f78d70dc2ec0c91752c145a60758e4ffd4
nbjmcclellan <nbjmcclellan@gmail.com>
2015-07-30 19:32:40 +0000

    new stuff for tables

--- application_controller.rb
+++ application_controller.rb
@@ -3,19 +3,31 @@ Bundler.require
 
 class MyApp < Sinatra::Base
 
+
+  
+  
+  
+  
+  
   get '/' do
     erb :index
   end
 
   get '/result' do
-    erb :result
+   @new = {
+  
+     :one => {:rating => "1", :name => "sidharth", :adress => "here", :mobile_url => "526826362", :phone => "36383739"},
+     :two => {:rating => "3", :name => "noah", :adress => "here", :mobile_url => "7705", :phone => "5127059410"},
+     :three => {:rating => "9", :name => "jon", :adress => "here", :mobile_url => "73945", :phone => "82694629"}
+  
+  
+     }
+   erb :result
   end
 
 get '/data' do
   erb :data
 end
-
-
 
 
 
---
+++ models/model.rb
@@ -0,0 +1,10 @@
+new = {
+  
+  :one => {:rating => "6", :name => "me", :mobile_url => "526826362", :phone => "36383739"}
+  :two => {:rating => "", :name => "", :mobile_url => "", :phone => ""}
+  :three => {:rating => "", :name => "", :mobile_url => "", :phone => ""}
+  
+  
+}
+
+
